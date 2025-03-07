c64a171d378ab5537e812b63f6167122e1164693
john <marcelo.1850518@studenti.uniroma1.it>
2025-03-07 16:12:04 +0000

    Prova canvas con mappa diag

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -4,11 +4,20 @@
 class Canvas : public rclcpp::Node {
 public:
     Canvas() : Node("canvas") {
-        canvas_ = cv::Mat::zeros(500,500,CV_8UC3);
-        //set for gray CV_8UC3
-        cv::imshow("Blank Canvas", canvas_);
-        RCLCPP_INFO(this->get_logger(),"Nodo di canva caricato");
-        
+        std::string map_path = "/home/john/Desktop/rp_ros2_rviz/map/cappero_laser_odom_diag_2020-05-06-16-26-03.png";
+
+        canvas_ = cv::imread(map_path, cv::IMREAD_COLOR);
+        if (canvas_.empty()) {
+            RCLCPP_ERROR(this->get_logger(), "Impossibile caricare la mappa dal file: %s", map_path.c_str());
+            return;
+        }
+        int new_height = 500;  
+        float aspect_ratio = static_cast<float>(canvas_.cols) / canvas_.rows;
+        int new_width = static_cast<int>(new_height * aspect_ratio);
+        cv::resize(canvas_, canvas_, cv::Size(new_width,new_height));
+        cv::imshow("Map Canvas", canvas_);
+        RCLCPP_INFO(this->get_logger(), "Nodo di canvas caricato");
+
         timer_ = this->create_wall_timer(
             std::chrono::milliseconds(100), 
             std::bind(&Canvas::update_canvas, this)
@@ -19,10 +28,9 @@ public:
 private:
     cv::Mat canvas_;
     rclcpp::TimerBase::SharedPtr timer_;  
-
     void update_canvas() {
-        cv::circle(canvas_, cv::Point(250, 250), 100, cv::Scalar(0, 0, 255), 2); 
-        cv::imshow("Blank Canvas", canvas_);
+
+        cv::imshow("Map Canvas", canvas_);
         
         cv::waitKey(1);  
     }
